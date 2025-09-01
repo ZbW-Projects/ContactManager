dfb7ac5d41fc2982fc6060ae8f45635ad04aaa50
EdiAnderegg <93870106+EdiAnderegg@users.noreply.github.com>
2025-09-01 17:23:13 +0200

    Problem mit der Standard-Funktion namensgebung bei automatischer Aufbau der Klasse Test

--- src/ContactManager.Test/Test.Model/Test.Person.cs
+++ src/ContactManager.Test/Test.Model/Test.Person.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ContactManager.Test.Test.Model
+namespace ContactManager.Test.Model
 {
-    internal class Test
+    internal class Person
     {
     }
 }
