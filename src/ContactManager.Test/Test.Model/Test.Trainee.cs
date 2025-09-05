cef4635dbfa77b8aaf3613ea83560488a104ec70
jro <jro@EP127>
2025-09-05 08:10:14 +0200

    Test erstellt Trainee

---
+++ src/ContactManager.Test/Test.Model/Test.Trainee.cs
@@ -0,0 +1,113 @@
+﻿using ContactManager.Core.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ContactManager.Tests.Core.Model
+{
+    [TestClass]
+    public class TraineeTests
+    {
+        [TestMethod]
+        public void EndDate_ValidDate_IsSet()
+        {
+            var t = new Trainee();
+            var end = new DateTime(2025, 6, 30);
+
+            t.EndDate = end;
+
+            Assert.AreEqual(end, t.EndDate);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void EndDate_Default_Throws()
+        {
+            var t = new Trainee();
+            t.EndDate = default;
+        }
+
+        [TestMethod]
+        public void CadreLevel_IsAlwaysZero()
+        {
+            var t = new Trainee();
+            t.CadreLevel = 3; // egal was gesetzt wird
+            Assert.AreEqual(0, t.CadreLevel);
+        }
+
+        [TestMethod]
+        public void TraineeYears_ValidBetween0And4_IsSet()
+        {
+            var t = new Trainee();
+            t.TraineeYears = 3;
+
+            Assert.AreEqual(3, t.TraineeYears);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TraineeYears_LessThanZero_Throws()
+        {
+            var t = new Trainee();
+            t.TraineeYears = -1;
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TraineeYears_GreaterThan4_Throws()
+        {
+            var t = new Trainee();
+            t.TraineeYears = 5;
+        }
+
+        [TestMethod]
+        public void ActualTraineeYear_UsesDatesDiff()
+        {
+            var start = new DateTime(DateTime.Today.Year - 2, 1, 1);
+            var end = DateTime.Today.AddYears(1); // noch nicht beendet
+            var t = new Trainee();
+            // Zugriff auf protected Felder _startDate/_endDate geht nur über Basisklasse Employee
+            // hier simulieren wir über Properties
+            t.EndDate = end;
+            var actual = DatesDiff.Year(start, end);
+
+            Assert.AreEqual(actual, t.ActualTraineeYear);
+        }
+    }
+
+    [TestClass]
+    public class DatesDiffTests
+    {
+        [TestMethod]
+        public void Year_TodayBeforeStart_Returns0()
+        {
+            var start = DateTime.Today.AddDays(1);
+            var result = DatesDiff.Year(start);
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void Year_TodayAfterEnd_ReturnsDifference()
+        {
+            var start = new DateTime(2020, 1, 1);
+            var end = new DateTime(2022, 1, 1);
+            var result = DatesDiff.Year(start, end);
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void Year_SameYear_Returns1()
+        {
+            var start = DateTime.Today.AddMonths(-2);
+            var result = DatesDiff.Year(start);
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void Year_DefaultEnd_UsesToday()
+        {
+            var start = new DateTime(DateTime.Today.Year - 5, 1, 1);
+            var result = DatesDiff.Year(start);
+            Assert.AreEqual(5, result);
+        }
+    }
+}
