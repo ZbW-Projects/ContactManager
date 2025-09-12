7d710f57dfecef8bc23c45bbb761c7e68c5dbf86
EdiAnderegg <93870106+EdiAnderegg@users.noreply.github.com>
2025-09-12 10:59:39 +0200

    Der kleine logische Fehler bei „Employee => Employment” wurde behoben. Es wurde von „value ≥ 100” zu „value > 100” geändert.

--- src/ContactManager.Core/Model/Employee.cs
+++ src/ContactManager.Core/Model/Employee.cs
@@ -36,7 +36,7 @@ namespace ContactManager.Core.Model
         public string Department { get => _department; set => _department = string.IsNullOrWhiteSpace(value) ? throw new ArgumentException("Das Departement muss vorhanden sein.") : Name.Normalize(value); }
         public DateTime StartDate { get => _startDate; set => _startDate = value == default ? throw new ArgumentException("Das StartDatum muss einen Wert enthalten.", nameof(value)) : value; }
         public virtual DateTime EndDate { get => _endDate; set => _endDate = value; }
-        public int Employment { get => _empolyment; set => _empolyment = value % 20 != 0 || value >= 100 || value <= 0 ? throw new ArgumentException("Der Beschäftigungsgrad ist nicht richtig", nameof(value)) : value; }
+        public int Employment { get => _empolyment; set => _empolyment = value % 20 != 0 || value > 100 || value <= 0 ? throw new ArgumentException("Der Beschäftigungsgrad ist nicht richtig", nameof(value)) : value; }
         public string Role { get => _role; set => _role = string.IsNullOrWhiteSpace(value) ? throw new ArgumentException("Rolle darf nicht leer sein.") : Name.Normalize(value); }
         public virtual int CadreLevel { get => _cadreLevel; set => _cadreLevel = value < 0 || value > 5 ? throw new ArgumentException("Der CadreLevel ist nicht richtig", nameof(value)) : value; }
 
